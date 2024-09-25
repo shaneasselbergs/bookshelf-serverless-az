48168c5ede4bf7e0939c9109d0532503d0707cea
shaneasselbergs <shaneasselbergs@gmail.com>
2024-09-25 15:07:34 +0200

    Create Azure Function from template

---
+++ BookHttpTrigger.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace My.Function
+{
+  public class BookHttpTrigger
+  {
+    private readonly ILogger<BookHttpTrigger> _logger;
+
+    public BookHttpTrigger(ILogger<BookHttpTrigger> logger)
+    {
+      _logger = logger;
+    }
+
+    [Function("BookHttpTrigger")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+      _logger.LogInformation("C# HTTP trigger function processed a request.");
+      return new OkObjectResult("Welcome to Azure Functions!");
+    }
+  }
+}
